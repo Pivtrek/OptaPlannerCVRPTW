c16b5a757941909a1230ea5d414dc914b7327693
Pivtrek <piotrek.bijacz.7@gmail.com>
2025-03-06 16:15:08 +0100

    Deleted unnecesery imports

--- src/main/java/com/example/routeplaner/config/SecurityConfig.java
+++ src/main/java/com/example/routeplaner/config/SecurityConfig.java
@@ -3,9 +3,6 @@ package com.example.routeplaner.config;
 import com.example.routeplaner.utils.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -38,7 +35,6 @@ public class SecurityConfig {
                 .cors(withDefaults())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
-                        //.requestMatchers("/api/**").authenticated()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
--- src/main/java/com/example/routeplaner/controller/VehicleController.java
+++ src/main/java/com/example/routeplaner/controller/VehicleController.java
@@ -1,19 +1,10 @@
 package com.example.routeplaner.controller;
-
-import com.example.routeplaner.model.User;
 import com.example.routeplaner.model.Vehicle;
-import com.example.routeplaner.repository.UserRepository;
 import com.example.routeplaner.service.VehicleService;
 import com.example.routeplaner.utils.JwtUtil;
-import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
-
-import java.security.Principal;
 import java.util.List;
 
 @RestController
@@ -44,11 +35,4 @@ public class VehicleController {
         vehicleService.deleteVehicle(id);
         return ResponseEntity.noContent().build();
     }
-
-//    @PutMapping("/{id}")
-//    public ResponseEntity<Vehicle> updateVehicle(@PathVariable Long id, @RequestBody Vehicle updatedVehicle) {
-//        Vehicle vehicle = vehicleService.updateVehicle(id, updatedVehicle);
-//        return ResponseEntity.ok(vehicle);
-//    }
-
 }
